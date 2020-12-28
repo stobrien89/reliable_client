a56e3f94d7d0747c1fb44d3a41892fb6b51ebaf5
stobrien89 <stobrien@crimson.ua.edu>
2020-12-28 14:15:22 -0800

    adjust margin on routes

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Services = () => {
     return <div class="container main-content">
-        <div className="row">
+        <div className="row mt-5">
             <div className="col-md-12">
                 <h2 class="mb-4">Services</h2>
                 <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
             </div>
         </div>
         <hr/>
-        <div className="row mb-4">
+        <div className="row mb-5 mt-4">
             <div className="col-md-4">
                 <div className="row d-flex justify-content-center">
                     <i className="fa fa-bath fa-lg"></i>
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Testimonials = () => {
     return <div class="container main-content">
-        <div className="row">
+        <div className="row mt-5">
             <div className="col-md-12">
                 <h2 class="mb-4">Testimonials</h2>
                 <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
