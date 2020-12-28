b66ca8d62d7ebb4d2699d2c2eff713215177a99e
stobrien89 <stobrien@crimson.ua.edu>
2020-12-27 18:28:53 -0800

    add testimonials layout

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -9,6 +9,24 @@ const Testimonials = () => {
             </div>
         </div>
         <hr/>
+        <div className="row mb-5">
+            <div className="col-md-6">
+                <p>"I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."</p>
+                <p><strong>-John Doe</strong></p>
+            </div>
+            <div className="col-md-6">
+                <p>"I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."</p>
+                <p><strong>-John Doe</strong></p>
+            </div>
+            <div className="col-md-6">
+                <p>"I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."</p>
+                <p><strong>-John Doe</strong></p>
+            </div>
+            <div className="col-md-6">
+                <p>"I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."</p>
+                <p><strong>-John Doe</strong></p>
+            </div>
+        </div>
     </div>
 }
 
