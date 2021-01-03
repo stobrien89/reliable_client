186bde1d2d1f2e66c387c5411e39de7f50ade515
stobrien89 <stobrien@crimson.ua.edu>
2021-01-02 17:21:53 -0800

    add content to contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,17 +5,33 @@ const Contact = () => {
         <div className="row mt-5">
             <div className="col-md-12">
                 <h2 class="mb-4">Contact</h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <p class="lead">Regardless of the size of your home or business, maintaining it can be a struggle. While you may not have the time or the tools to accomplish everything on your to-do list, what you do have is a <em>reliable</em> handyman at your service. Call or email today for a free estimate!</p>
             </div>
         </div>
         <hr/>
         <div className="row mb-5">
-            <div className="col-md-6">
-                <h5>Email:</h5>
-                <h5>Phone:</h5>
+            <div className="col-md-6 mb-3 text-center">
+                <h4>Phone:</h4>
+                <p>
+                    <a href="tel:+1-954-913-8273">(954) 913-8273</a>
+                </p>
+                <h4>Email:</h4>
+                <p>
+                    <a href={"mailto:reliablehomesrvc@gmail.com"}>reliablehomesrvc@gmail.com</a>
+                </p>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 text-center">
                 <h4>Areas Covered:</h4>
+                <ul className="list-group list-group-flush text-center">
+                    <li className="list-group-item">Boca Raton</li>
+                    <li className="list-group-item">Boynton Beach</li>
+                    <li className="list-group-item">Deerfield Beach</li>
+                    <li className="list-group-item">Fort Lauderdale</li>
+                    <li className="list-group-item">Hollywood</li>
+                    <li className="list-group-item">Delray</li>
+                    <li className="list-group-item">Lake Worth</li>
+                    <li className="list-group-item">West Palm Beach</li>
+                </ul>
             </div>
         </div>
     </div>
